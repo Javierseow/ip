599afe26ac4314e8f252b12fabfa747927d1d509
Javierseow <seowjackiejavier@gmail.com>
2026-01-29 23:26:31 +0800

    Add list function

--- src/main/java/Jackson.java
+++ src/main/java/Jackson.java
@@ -1,14 +1,35 @@
 import java.util.Scanner;
 
 public class Jackson {
-    public static void main(String[] args) {
-        String line = "";
 
+    public static void printList(String[] items, int itemCount) {
+        if (itemCount == 0) {
+            System.out.println("Your list is empty bro");
+        } else {
+            for (int i = 0; i < itemCount; i++) {
+                System.out.print((i+1) + ". ");
+                System.out.println(items[i]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Hello, I'm Jackson");
-        System.out.print("What can I do for you?");
+        System.out.println("What can I do for you?");
+
+        String line = in.nextLine();
+        String[] items = new String[100];
+        int itemCount = 0;
+
         while (!line.equals("bye")) {
-            System.out.println(line);
+            if (line.equals("list")) {
+                printList(items, itemCount);
+            } else {
+                System.out.println("Added: " + line);
+                items[itemCount] = line;
+                itemCount++;
+            }
             line = in.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
