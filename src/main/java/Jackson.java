ed61b0611c809af17c6507caf4712601d30bb090
Javierseow <seowjackiejavier@gmail.com>
2026-01-29 22:31:51 +0800

    Add echo

--- src/main/java/Jackson.java
+++ src/main/java/Jackson.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class Jackson {
     public static void main(String[] args) {
+        String line;
+        Scanner in = new Scanner(System.in);
         System.out.println("Hello, I'm Jackson");
         System.out.println("What can I do for you?");
+        do {
+            line = in.nextLine();
+            System.out.println(line);
+        } while (!line.equals("bye"));
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
