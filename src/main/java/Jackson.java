07383595d60a9c13cefd87c8b523002e0c1969c8
Javierseow <seowjackiejavier@gmail.com>
2026-01-29 22:17:21 +0800

    Remove horizontal lines

--- src/main/java/Jackson.java
+++ src/main/java/Jackson.java
@@ -1,10 +1,7 @@
 public class Jackson {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
         System.out.println("Hello, I'm Jackson");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
     }
 }
