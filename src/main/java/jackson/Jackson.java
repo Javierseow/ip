68a1ed51118041282acc534a2ec062bf3292c08a
Javierseow <seowjackiejavier@gmail.com>
2026-02-19 18:23:40 +0800

    Change from Task[] array to ArrayList<Task>

--- src/main/java/jackson/Jackson.java
+++ src/main/java/jackson/Jackson.java
@@ -1,6 +1,7 @@
 package jackson;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 import jackson.task.Task;
 import jackson.task.Todo;
 import jackson.task.Deadline;
@@ -8,9 +9,8 @@ import jackson.task.Event;
 import jackson.exception.JacksonException;
 
 public class Jackson {
-    static final int MAX_SIZE = 100;
 
-    public static void printList(Task[] items) {
+    public static void printList(ArrayList<Task> items) {
         if (Task.getTaskCount() == 0) {
             System.out.println("Your list is empty bro");
             return;
@@ -19,21 +19,21 @@ public class Jackson {
         System.out.println("Here's your list bro");
         for (int i = 0; i < Task.getTaskCount(); i++) {
             System.out.print((i + 1) + ".");
-            System.out.println(items[i]);
+            System.out.println(items.get(i));
         }
     }
 
-    public static void updateMarkStatus(Task[] items, String instruction, int taskNumber) {
+    public static void updateMarkStatus(ArrayList<Task> items, String instruction, int taskNumber) {
         if (instruction.startsWith("mark")) {
-            items[taskNumber - 1].markAsDone();
+            items.get(taskNumber - 1).markAsDone();
         } else {
-            items[taskNumber - 1].markAsUndone();
+            items.get(taskNumber - 1).markAsUndone();
         }
     }
 
-    public static void addTask(Task[] items, String taskType, String description) throws JacksonException{
+    public static void addTask(ArrayList<Task> items, String taskType, String description) throws JacksonException{
         if (taskType.equals("todo")) {
-            items[Task.getTaskCount()] = new Todo(description);
+            items.add(new Todo(description));
             return;
         }
 
@@ -46,8 +46,7 @@ public class Jackson {
             if (taskInfo.length < 2 || !taskInfo[1].trim().startsWith("by ")) {
                 throw new JacksonException("Yo u forgot to specify ur deadline time");
             }
-            items[Task.getTaskCount()] = new Deadline(taskInfo[0].trim(),
-                    taskInfo[1].trim().substring(3).trim());
+            items.add(new Deadline(taskInfo[0].trim(), taskInfo[1].trim().substring(3).trim()));
             return;
         }
 
@@ -60,9 +59,9 @@ public class Jackson {
         if (!taskInfo[2].trim().startsWith("to ")) {
             throw new JacksonException("Gotta tell me when your event ends bro");
         }
-        items[Task.getTaskCount()] = new Event(taskInfo[0].trim(),
+        items.add(new Event(taskInfo[0].trim(),
                 taskInfo[1].trim().substring(5).trim(),
-                taskInfo[2].trim().substring(3).trim());
+                taskInfo[2].trim().substring(3).trim()));
     }
 
     public static void main(String[] args) {
@@ -71,7 +70,7 @@ public class Jackson {
         System.out.println("What can I do for you?");
 
         String line = in.nextLine();
-        Task[] items = new Task[MAX_SIZE];
+        ArrayList<Task> items = new ArrayList<>();
 
         while (!line.equals("bye")) {
             String[] words = line.trim().split(" ", 2);
@@ -93,7 +92,7 @@ public class Jackson {
                     String status = line.startsWith("mark") ? "done" : "not done yet";
                     System.out.println("Okay, I've marked task " + taskNumber + " as " + status);
                     System.out.print("  ");
-                    System.out.println(items[taskNumber - 1]);
+                    System.out.println(items.get(taskNumber - 1));
                 } catch (ArrayIndexOutOfBoundsException e) {
                     System.out.println("Pls provide a task number to mark");
                 } catch (NumberFormatException e) {
@@ -114,7 +113,7 @@ public class Jackson {
 
                     System.out.println("Got it. I've added this task:");
                     System.out.print("  ");
-                    System.out.println(items[Task.getTaskCount() - 1]);
+                    System.out.println(items.get(items.size() - 1));
                     System.out.print("Now you have " + Task.getTaskCount() + " task");
                     if (Task.getTaskCount() > 1) {
                         System.out.print("s");
